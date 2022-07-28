158e803a97814a63a27dce7bf96788b63d7e3378
Sarah Silva Lopes <92373290+sarahslopes@users.noreply.github.com>
2022-07-28 17:30:37 -0300

    Http Status

--- src/main/java/com/jpa/demo/controller/ProductController.java
+++ src/main/java/com/jpa/demo/controller/ProductController.java
@@ -1,10 +1,12 @@
-package com.jpa.demo.controller;
+ackage com.jpa.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +15,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.jpa.demo.model.Product;
 import com.jpa.demo.repository.ProductInterface;
@@ -28,6 +32,7 @@ public class ProductController {
     @Autowired ProductInterface repo;
 
     @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
     @ApiOperation("Cadastra produto")
     public @ResponseBody Product saveCustomer(@Valid Product product) {
        repo.save(product);
@@ -42,11 +47,18 @@ public class ProductController {
 
     @GetMapping(path ="/list/{id}")
     @ApiOperation("Lista produto por id")
-    public Object listById(@PathVariable Integer id){
-        return repo.findById(id);
+    public Product listById(@PathVariable Integer id){
+    Optional<Product> product = repo.findById(id);
+
+        if(product.isPresent()){
+            return product.get();
+        }
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado");
     }
 
     @PutMapping(path = "/update/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @ApiOperation("Atualiza produto por id")
     public Product updateCustomer(@Valid Product product){
         saveCustomer(product);
@@ -54,8 +66,11 @@ public class ProductController {
     }
 
     @DeleteMapping(path = "/delete/{id}" )
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @ApiOperation("Deleta produto por id")
     public void deleteCustomer(@PathVariable Integer id){
+        repo.findById(id);
+
         repo.deleteById(id);
     }
 }
